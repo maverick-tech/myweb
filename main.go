6d33e966a2ab406e7b8e813e1b4b51475198fedb
maverick-tech <tarlemanmohan@outlook.com>
2020-06-26 20:51:56 +0530

    added feature 2

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func webHandlerFunc(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprint(writer, "<h1>Hello, World</h1> <h3>This is new feature</h3>")
+	fmt.Fprint(writer, "<h1>Hello, World</h1> <h3>This is new feature</h3> <h3> This is feature 2</h3>")
 }
 
 func GetPort() (port string){
